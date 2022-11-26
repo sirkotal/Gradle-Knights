a2c1ed539de4d04feaaf2c0b70bc22bc47c5cb06
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2022-11-26 12:30:07 +0000

    hotfix

--- src/main/java/pt/up/fe/ldts/gd/model/Item.java
+++ src/main/java/pt/up/fe/ldts/gd/model/Item.java
@@ -28,5 +28,18 @@ public abstract class Item {
         return this.value;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == this)
+            return true;
+
+        if(!(obj instanceof Item))
+            return false;
+
+        Item i = (Item) obj;
+
+        return name.equals(i.getName());
+    }
+
     public abstract void setUsed();
 }
