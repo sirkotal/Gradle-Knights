309f9a6a07850d17d016b01a06acb892bd172995
sirkotal <lordkotal2002@gmail.com>
2022-12-07 18:47:21 +0000

    Created the State class

--- src/main/java/pt/up/fe/ldts/gd/Game.java
+++ src/main/java/pt/up/fe/ldts/gd/Game.java
@@ -1,6 +1,8 @@
 package pt.up.fe.ldts.gd;
+import pt.up.fe.ldts.gd.state.State;
 
 public class Game {
+
     public Game() {
         System.out.println("Welcome to Gradle Knights!");
     }
---
+++ src/main/java/pt/up/fe/ldts/gd/state/State.java
@@ -0,0 +1,22 @@
+package pt.up.fe.ldts.gd.state;
+
+public abstract class State<T> {
+    private final T template;
+    private final Controller<T> controller;
+    private final Viewer<T> viewer;
+
+    public State(T template) {
+        this.template = template;
+        this.viewer = getViewer();
+        this.controller = getController();
+    }
+
+    public T getTemplate() {
+        return this.template;
+    }
+
+    protected abstract Viewer<T> getViewer();
+
+    protected abstract Controller<T> getController();
+
+}
