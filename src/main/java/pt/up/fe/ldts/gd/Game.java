228c8cffee71e14e683b3bcddeb7c3888a01e3e6
sirkotal <lordkotal2002@gmail.com>
2022-12-07 18:48:21 +0000

    Added State to the main class (Game)

--- src/main/java/pt/up/fe/ldts/gd/Game.java
+++ src/main/java/pt/up/fe/ldts/gd/Game.java
@@ -2,9 +2,14 @@ package pt.up.fe.ldts.gd;
 import pt.up.fe.ldts.gd.state.State;
 
 public class Game {
+    private State state;
 
     public Game() {
         System.out.println("Welcome to Gradle Knights!");
+    }
+
+    public void setState(State state) {
+        this.state = state;
     }
 
     public static void main(String[] args) {
