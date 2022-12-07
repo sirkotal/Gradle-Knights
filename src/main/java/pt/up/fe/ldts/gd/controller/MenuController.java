9d622c135d21c410531a19e7631cbe41f1c71b48
sirkotal <lordkotal2002@gmail.com>
2022-12-07 21:35:53 +0000

    Added the MenuController class (requires Menu)

---
+++ src/main/java/pt/up/fe/ldts/gd/controller/MenuController.java
@@ -0,0 +1,15 @@
+package pt.up.fe.ldts.gd.controller;
+
+import pt.up.fe.ldts.gd.Game;
+import pt.up.fe.ldts.gd.model.menu.Menu;
+import pt.up.fe.ldts.gd.model.wild.Wild;
+
+public class MenuController extends Controller<Menu> {
+    public MenuController(Menu menu) {
+        super(menu);
+    }
+
+    public void step(Game game/*, GUI action*/) {
+
+    }
+}
