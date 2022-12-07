a9cb9c98d690e13273eca12fe24ccd8d1a100515
sirkotal <lordkotal2002@gmail.com>
2022-12-07 21:28:11 +0000

    Added TownController and WildController classes

---
+++ src/main/java/pt/up/fe/ldts/gd/controller/TownController.java
@@ -0,0 +1,14 @@
+package pt.up.fe.ldts.gd.controller;
+
+import pt.up.fe.ldts.gd.Game;
+import pt.up.fe.ldts.gd.model.town.Town;
+
+public class TownController extends Controller<Town> {
+    public TownController(Town town) {
+        super(town);
+    }
+
+    public void step(Game game/*, GUI action*/) {
+
+    }
+}
---
+++ src/main/java/pt/up/fe/ldts/gd/controller/WildController.java
@@ -0,0 +1,14 @@
+package pt.up.fe.ldts.gd.controller;
+
+import pt.up.fe.ldts.gd.Game;
+import pt.up.fe.ldts.gd.model.wild.Wild;
+
+public class WildController extends Controller<Wild> {
+    public WildController(Wild wild) {
+        super(wild);
+    }
+
+    public void step(Game game/*, GUI action*/) {
+
+    }
+}
