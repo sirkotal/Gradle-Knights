2ac6b511fc1f6acf62a5c31732274444231a4d09
sirkotal <lordkotal2002@gmail.com>
2022-12-14 11:44:33 +0000

    Added an AmuletItem class

---
+++ src/main/java/pt/up/fe/ldts/gd/model/player/AmuletItem.java
@@ -0,0 +1,26 @@
+package pt.up.fe.ldts.gd.model.player;
+
+import pt.up.fe.ldts.gd.model.town.CheapStrategy;
+import pt.up.fe.ldts.gd.model.town.ExpensiveStrategy;
+import pt.up.fe.ldts.gd.model.town.Shop;
+
+public class AmuletItem extends Item {
+    private CheapStrategy lowstrat;
+    private ExpensiveStrategy highstrat;
+    public AmuletItem(String name, int value, int price) {
+        super(name, value, price);
+    }
+
+    public void changeStrategy(Shop shop) {
+        if (shop.getStrategy() instanceof CheapStrategy) {
+            shop.setStrategy(new ExpensiveStrategy());
+        }
+        else if (shop.getStrategy() instanceof ExpensiveStrategy) {
+            shop.setStrategy(new CheapStrategy());
+        }
+    }
+
+    public void setUsed() {
+        this.used = true;
+    }
+}
