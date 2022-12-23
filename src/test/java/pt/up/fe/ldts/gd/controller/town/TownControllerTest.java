b5fa09714d3690cfa84c87783070c110e3dedb96
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2022-12-23 21:53:20 +0000

    killed some mutations on town controller

--- src/test/java/pt/up/fe/ldts/gd/controller/town/TownControllerTest.java
+++ src/test/java/pt/up/fe/ldts/gd/controller/town/TownControllerTest.java
@@ -1,41 +1,43 @@
 package pt.up.fe.ldts.gd.controller.town;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import pt.up.fe.ldts.gd.Game;
-import pt.up.fe.ldts.gd.controller.menu.MenuController;
 import pt.up.fe.ldts.gd.gui.GUI;
-import pt.up.fe.ldts.gd.model.menu.Menu;
-import pt.up.fe.ldts.gd.model.player.Player;
 import pt.up.fe.ldts.gd.model.town.Shop;
 import pt.up.fe.ldts.gd.model.town.Town;
-import pt.up.fe.ldts.gd.model.wild.Wild;
 import pt.up.fe.ldts.gd.state.menu.MenuState;
 import pt.up.fe.ldts.gd.state.town.ShopState;
-import pt.up.fe.ldts.gd.state.town.TownState;
 import pt.up.fe.ldts.gd.state.wild.WildState;
 
 import java.io.IOException;
 
 public class TownControllerTest {
     private TownController controller;
-    private Town town;
     private Game game;
-    private Player player;
 
     @BeforeEach
     public void setup() throws IOException {
-        player = Mockito.mock(Player.class);
-        town = new Town(player);
-        controller = new TownController(town);
+        Town town = Mockito.mock(Town.class);
+        controller = Mockito.spy(new TownController(town));
         game = Mockito.mock(Game.class);
     }
 
     @Test
-    public void townToShop() throws IOException {
+    public void townToExistingShop() throws IOException {
         Mockito.when(game.getPreviousState()).thenReturn(Mockito.mock(ShopState.class));
+        controller.step(game, GUI.ACTION.OPT1);
+
+        Mockito.verify(game, Mockito.times(1)).setState(Mockito.any(ShopState.class));
+        Mockito.verify(game, Mockito.times(1)).setState(Mockito.any());
+    }
+
+    @Test
+    public void townToNewShop() throws IOException {
+        Mockito.when(game.getPreviousState()).thenReturn(Mockito.mock(WildState.class));
+        Mockito.when(controller.getModel()).thenReturn(Mockito.mock(Town.class));
+        Mockito.when(controller.getModel().getShop()).thenReturn(Mockito.mock(Shop.class));
         controller.step(game, GUI.ACTION.OPT1);
 
         Mockito.verify(game, Mockito.times(1)).setState(Mockito.any(ShopState.class));
