9bef33787868dc420a2277819a4dd3a1fcd540b0
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2022-12-23 00:06:59 +0000

    small improvement to inventory model tests

--- src/test/java/pt/up/fe/ldts/gd/model/player/InventoryTest.java
+++ src/test/java/pt/up/fe/ldts/gd/model/player/InventoryTest.java
@@ -20,12 +20,21 @@ public class InventoryTest {
     public void addAndRemoveItems() {
         CombatItem katana = new CombatItem("Uchigatana", 100, 5000);
         PotionItem potion = new PotionItem("Blood God's Offering", 200, 3500);
+
+        Assertions.assertEquals(2, inventory.getOptions().size());
+
         inventory.addItem(katana);
+        Assertions.assertEquals(3, inventory.getOptions().size());
+
         inventory.addItem(potion);
+        Assertions.assertEquals(4, inventory.getOptions().size());
+
         Assertions.assertEquals(2, inventory.size());
         Assertions.assertEquals("Blood God's Offering", inventory.getItem(1).getName());
         Assertions.assertFalse(inventory.isEmpty());
-        inventory.removeItem(potion);
+
+        inventory.removeItem(katana);
+        Assertions.assertEquals(3, inventory.getOptions().size());
         Assertions.assertEquals(1, inventory.size());
     }
 
@@ -68,5 +77,44 @@ public class InventoryTest {
 
         Assertions.assertEquals(0, inventory.size());
         Assertions.assertEquals(2, inventory.getOptions().size());
+    }
+
+    @Test
+    public void removeExistingPotion() {
+        PotionItem item = new PotionItem("Methylamine", 10000, 100000);
+
+        Assertions.assertEquals(0, inventory.size());
+        Assertions.assertEquals(2, inventory.getOptions().size());
+
+        inventory.addItem(item);
+        inventory.addItem(item);
+
+        Assertions.assertEquals(1, inventory.size());
+        Assertions.assertEquals(3, inventory.getOptions().size());
+        Assertions.assertEquals(2, inventory.getItem(0).getCount());
+
+        String before = inventory.getOptions().get(0);
+
+        inventory.removeItem(item);
+
+        Assertions.assertEquals(1, inventory.size());
+        Assertions.assertEquals(3, inventory.getOptions().size());
+        Assertions.assertEquals(1, inventory.getItem(0).getCount());
+
+        Assertions.assertNotEquals(inventory.getOptions().get(0), before);
+    }
+
+    @Test
+    public void equippedRefreshOptions() {
+        CombatItem katana = new CombatItem("Uchigatana", 100, 5000);
+        inventory.addItem(katana);
+
+        String before = inventory.getOptions().get(0);
+        katana.setUsed(true);
+        inventory.refreshOptions();
+        Assertions.assertNotEquals(inventory.getOptions().get(0), before);
+        katana.setUsed(false);
+        inventory.refreshOptions();
+        Assertions.assertEquals(inventory.getOptions().get(0), before);
     }
 }
